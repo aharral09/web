2f4bda3bb8c8ac2ce72a49b0c13467748354dfde
aharral09 <aharral09@gmail.com>
2021-04-22 22:07:33 -0400

    started learning Vue.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+app.message = 'I have changed the data!';
+
+var app2 = new Vue({
+    el: "#app-2",
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleTimeString()
+    }
+})
